7891c8eb14b8d2906c65f0f3e77e076ff50e785b
ocalderon <calderon_edsel@hotmail.com>
2019-04-29 22:21:23 -0500

    eli

--- Contenido2.cs
+++
@@ -1,7 +0,0 @@
-<?php
-echo "Bienvenido";
-echo "Bienvenido 2";
-echo "Bienvenido 3";
-echo "Bienvenido 7";
-echo "Bienvenido 5";
-?>
--- Contenido4.cs
+++
@@ -1,8 +0,0 @@
-<?php
-echo "Bienvenido";
-echo "Bienvenido 2";
-echo "Bienvenido 3";
-echo "Bienvenido 7";
-echo "Bienvenido 5";
-echo "Bienvenido 10";
-?>
